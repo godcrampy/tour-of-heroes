4f625e8b7f6fe62b2126d17625f02ad54a57e206
Sahil Bondre <sahilbondre@gmail.com>
2018-12-21 17:09:33 +0530

    Added Message and Hero Services. Message service displays command carried put in the message template. Hero service gets the mock heroes from un an asynchronized way and injects into heros component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import {Hero} from './hero.model'
 import {HEROES} from './mock-heroes'
 import {Observable, of} from 'rxjs'
+import {MessageService} from './message.service'
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
+    this.messageService.addMessage('Hero Service: Fetched Heroes')
     return of(HEROES);
   }
 }
